e1f649c59db29333688251b773fd33036f5369a6
EstherVianna <esther.avianna@gmail.com>
2023-02-11 13:44:09 -0300

    disable input function

--- Euro/euro.js
+++ Euro/euro.js
@@ -1,29 +1,63 @@
-function Convert(){
-    const valorElemento = document.querySelector("#valor")
-    let valor = valorElemento.value
+function Convert() {
+  const coins = [
+    "EUR-BRL",
+    "USD-BRL",
+    "BTC-BRL",
+    "ARS-BRL",
+    "GBP-BRL"
+  ];
+  const url = "https://economia.awesomeapi.com.br/json/last/";
+  const formatCurrency = (value) => {
+      return value.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+    }
 
-    let conversao = (valor * 5.57).toFixed(100)
+  fetch(url + coins)
+    .then(function(response) {
+      if (!response.ok) {
+        throw new Error("Erro ao obter dados da API: " + response.statusText);
+      }
+      return response.json();
+    })
+    .then(function(data) {
+      const value = parseFloat(document.querySelector("#valor").value);
 
-    valor = parseFloat(valor)
-    conversao = parseFloat(conversao)
+      if(value > 0 ){
+      let euroReal = data.EURBRL;
+      const currency = euroReal.ask;
+      const elementoPai = document.querySelector(".container");
+      const elementoFilho = document.createElement("input");
+      const resposta = `${value} euros é igual a ${formatCurrency(value * currency)} reais`;
 
-    if(valor> 0 ){
-        const elementoPai = document.querySelector(".container");
-        let elementoFilho = document.createElement("input");
-        const  resposta = `${valor} euros é igual a ${conversao} reais`;
-        elementoFilho.id = "valor";
-        elementoFilho.setAttribute("disabled", null);
-        elementoPai.appendChild(elementoFilho);
-        elementoFilho.value = resposta;
+      elementoFilho.id = "resultado";
 
-        resposta;
+      elementoFilho.setAttribute("disabled", null);
+
+      elementoPai.appendChild(elementoFilho);
+
+      elementoFilho.value = resposta;
+      
+      const paragraph = document.createElement("p");
+      paragraph.id = "date"
+      const date =  new Date (euroReal.create_date);
+
+      const LastUpdate = paragraph.innerHTML =
+                       `Última atualização ${date.toLocaleString()}`;
+
+      elementoPai.appendChild(paragraph);
+
+      return resposta, LastUpdate;
     
-    setTimeout(()=>{
-        let counter = 0;
-        window.location.reload()
-        window.open(`A tela irá reiniciar em ${ counter++ }`)}, 20000);
     }
     else{
-        alert(`Insira um valor válido`)
-    }
+      alert(`Insira um valor válido`)}
+  })
+    .catch(function(error) {
+      console.error(error.message);
+    });
+
+    setTimeout(() => {
+      document.querySelector("#resultado").remove();
+      document.querySelector("#date").remove();
+      document.querySelector("#valor").value = "";
+    }, 30000);
 }
